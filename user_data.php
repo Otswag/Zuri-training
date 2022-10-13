9544ac8fd0429f42f7175dd919f116773c77f032
Otswag <106761383+Otswag@users.noreply.github.com>
2022-10-13 23:48:06 +0100

    User data

--- user_data.php
+++ user_data.php
@@ -1,1 +1,13 @@
+<?php
+$name =$_POST ["name"];
+$email =$_POST ["email"];
+$date =$_POST ["date"];
+$gender =$_POST ["gender"];
+$country =$_POST ["country"];
 
+echo "NAME: " , $name , "<br>"
+echo "EMAIL: " , $email , "<br>"
+echo "DATE OF BIRTH: " , $date , "<br>"
+echo "GENDER: " , $gender , "<br>"
+echo "COUNTRY: " , $country , "<br>"
+?>
